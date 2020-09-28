cf7d7b75b3cd4f101baa66c9d2ff692a722ded33
Ramazan774 <ramazan.ramazanov@glion.ch>
2020-09-28 14:04:18 -0500

    base

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,13 @@
 const dropper = document.querySelector(".dropper")
-const dropperHeight = dropper.style.bottom 
 
 function dropCoin(){
-    let height = dropperHeight.replace("px","")
-    height = parseInt(height, 10) - 25
-    dropper.style.bottom = `${height}px`
+    let height = dropper.style.bottom.replace("px","");
+    height = parseInt(height, 10);
+    if (height > 70) {
+        dropper.style.bottom = `${height - 25}px`
+    } else {
+        dropper.parentNode.innerHTML = "GAME OVER"
+    }
 }
 
 function dropCoinCounter() {
