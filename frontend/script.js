88d8d2f11a98404de86fdb94fbeb74a3ba9c0b86
KRStoddard <kimberlynrstoddard@gmail.com>
2020-09-29 17:08:33 -0500

    added game features

--- frontend/script.js
+++ frontend/script.js
@@ -3,60 +3,111 @@ const dropper1 = document.querySelector(".dropper1")
 const dropper2 = document.querySelector(".dropper2")
 const catcher = document.querySelector(".catcher")
 const points = document.querySelector(".points")
+let timers = {}
+dropper1.style.left = `${Math.floor(Math.random()* 450)}px`
+dropper2.style.left = `${Math.floor(Math.random()* 450)}px`
 
+function main() {
+    setTimer(dropper1, "one")
+    secondDropper(dropper2)
+    keyListener()
+}
+function getSpeed(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 function dropperSpeed(){
     let speed
     let pointsData = parseInt(points.innerText.split(' ')[1])
-    if (pointsData < 25){
-        speed = 750
-    } else if(pointsData >= 25 && pointsData < 50){
-        speed = 500
+    if (pointsData < 20){
+        speed = 700
+    } else if(pointsData >= 20 && pointsData < 50){
+        speed = getSpeed(500, 700)
     } else if(pointsData >= 50 && pointsData < 75){
-        speed = 250
+        speed = getSpeed(300, 700)
     } else {
-        speed = 100
+        speed = getSpeed(200, 700)
     }
     return speed
 }
-
-let timers = {}
-let timerId2 
 
 function setTimer(dropper, timerNum){
     clearInterval(timers[timerNum])
     timers[timerNum] = setInterval(function(){dropCoin(dropper, timerNum)}, dropperSpeed())
 }
 
-setTimer(dropper1, "one")
-
-setTimeout(function(){
-    setTimer(dropper2, "two")
-}, 5000)
-
-dropper1.style.left = `${Math.floor(Math.random()* 450)}px`
-dropper2.style.left = `${Math.floor(Math.random()* 450)}px`
+function secondDropper(dropper) {
+    setTimeout(function(dropper){
+        dropper.style.visibility = ""
+        setTimer(dropper, "two")
+    }, 7000)
+}
 
 function dropCoin(dropper, timerNum){
-    let height = dropper.style.bottom.replace("px","")
-    height = parseInt(height, 10)
+    let height = parseInt((dropper.style.bottom.replace("px","")), 10)
     if (height > 24){ 
-        dropper.style.bottom = `${height - 25}px`
+        keepDropping(height, dropper)
     }
     else if (height === 24 && withinRange(dropper)) {
-        clearInterval(timers[timerNum])
-        dropper.style.left = `${Math.floor(Math.random()* 450)}px`
-        dropper.style.bottom = "449px"
-        let pointsData = parseInt(points.innerText.split(' ')[1]) + 5
-        points.innerText = `Points: ${pointsData}`
-        setTimer(dropper, timerNum)
+        if (dropper.style.background === "black") {
+            gameOver(dropper)
+        } else if (dropper.style.background === "green") {
+            points.innerText = `Points: ${parseInt(points.innerText.split(' ')[1]) * 2 - 5}`
+            continueGame(timerNum, dropper)
+        } else {
+            continueGame(timerNum, dropper) 
+        }
     }
     else {
-        dropper.parentNode.innerHTML = "GAME OVER!"
-        clearInterval(timers["one"]) 
-        clearInterval(timers["two"]) 
+        if (dropper.style.background != "black") {
+        gameOver(dropper)
+        } else {
+            continueGame(timerNum, dropper)
+        }
     }
 }
 
+function keepDropping(height, dropper) {
+    dropper.style.bottom = `${height - 25}px`
+}
+
+function continueGame(timerNum, dropper) {
+    clearInterval(timers[timerNum])
+    dropper.style.left = `${Math.floor(Math.random()* 450)}px`
+    dropper.style.bottom = "449px" 
+    dropper.style.background = randomDropper()
+    points.innerText = `Points: ${parseInt(points.innerText.split(' ')[1]) + 5}`
+    setTimer(dropper, timerNum)
+}
+
+function gameOver(dropper) {
+    dropper.parentNode.innerHTML = "GAME OVER!"
+    clearInterval(timers["one"]) 
+    clearInterval(timers["two"]) 
+}
+
+function randomDropper() {
+    let randomColor = Math.random()
+    if (randomColor < .25) {
+        return "black"
+    }
+    else if (randomColor > .25 && randomColor < .4) {
+        return "green"
+    }
+    else {
+        return "linear-gradient(45deg,  rgb(201, 179, 15) 0%,rgba(255,255,255,1) 56%,rgb(211, 174, 10) 96%)"
+    }
+}
+
+function keyListener() {
+    document.addEventListener('keydown', function(e) {
+        if (e.key === "ArrowLeft") {
+            moveCatcherLeft()  
+        }
+        else if (e.key === "ArrowRight") {
+            moveCatcherRight()
+        }
+    })
+}
 function moveCatcherLeft() {
     var leftNumbers = catcher.style.left.replace('px', '');
     var left = parseInt(leftNumbers, 10)
@@ -71,23 +122,11 @@ function moveCatcherRight() {
     if (left < 440) {
       catcher.style.left = `${left + 20}px`
     }
-    }
-
-  document.addEventListener('keydown', function(e) {
-    if (e.key === "ArrowLeft") {
-      moveCatcherLeft()  
-    }
-    else if (e.key === "ArrowRight") {
-        moveCatcherRight()
-    }
-})
+}
 
 function withinRange(dropper){
-    
-    let catcherLeft = catcher.style.left.replace("px", "")
-    catcherLeft = parseInt(catcherLeft, 10)
-    let dropperLeft = dropper.style.left.replace("px", "")
-    dropperLeft = parseInt(dropperLeft, 10)
+    let catcherLeft = parseInt((catcher.style.left.replace("px", "")), 10)
+    let dropperLeft = parseInt((dropper.style.left.replace("px", "")), 10)
     let result
     let catcherLeftRange = []
     let dropperLeftRange = []
@@ -106,3 +145,5 @@ function withinRange(dropper){
     })
     return result 
 }
+
+main()
