c9c52e759c29bcda8579bf5adf250bc1c505c650
KRStoddard <kimberlynrstoddard@gmail.com>
2020-09-30 11:59:25 -0500

    added ramazan stuff

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -1,2 +1,9 @@
 class GamesController < ApplicationController
+
+    def create
+        game = Game.find_or_create_by(points: params[:points].to_i, user_id: params[:user_id].to_i)
+
+        render json: game
+    end
+    
 end
